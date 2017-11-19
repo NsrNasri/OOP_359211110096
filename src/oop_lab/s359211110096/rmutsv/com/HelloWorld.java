85c41292a6d97b7e813204389f5cef4f3af8d259
Nsrnasri <Nasri_093654@hotmail.com>
2017-11-19 10:43:33 +0700

    Add Information

--- src/oop_lab/s359211110096/rmutsv/com/HelloWorld.java
+++ src/oop_lab/s359211110096/rmutsv/com/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld
 {
 //    Creat main method
     public static void main(String[] args){
-      System.out.println("Hello world, I'm Nasri");
+      System.out.println("Hello world, I'm Nsrnasri");
       System.out.println("My name is Muhammadnasri sa-ih");
 //  add information
         System.out.println("Faculty:Management Technology");
