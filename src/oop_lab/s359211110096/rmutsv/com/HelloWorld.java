f8f80fe5e1a6e293f963674e4ca5ca4858405bde
kajonsak1161 <kajonsak1161>
2017-11-20 13:24:08 +0700

    Add Information

--- src/oop_lab/s359211110096/rmutsv/com/HelloWorld.java
+++ src/oop_lab/s359211110096/rmutsv/com/HelloWorld.java
@@ -3,11 +3,12 @@ package oop_lab.s359211110096.rmutsv.com;
 public class HelloWorld
 {
 //    Creat main method
-    public static void main(String[] args){
-      System.out.println("Hello world, I'm Nsrnasri");
-      System.out.println("My name is Muhammadnasri sa-ih");
+    public static void main(String[] args) {
+        System.out.println("Hello world, I'm Nsrnasri");
+        System.out.println("My name is Muhammadnasri sa-ih");
 //  add information
         System.out.println("Faculty:Management Technology,RMUTSV");
-    }//nain
+
+    }//main
 
 }//class
