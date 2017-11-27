b5e2db902f6202c407a9678f14a6752e87d30dd2
NsrNsr <Nasri_093654@hotmail.com>
2017-11-27 16:24:45 +0700

    Swich input

--- src/ooplab3/TestForLoop.java
+++ src/ooplab3/TestForLoop.java
@@ -4,8 +4,8 @@ public class TestForLoop {
     public static void main(String[] args) {
 
         for (int i =1;i<10;i++){
-            if (i%3==01||i%5==0||i%7==0)
-                System.out.println(i);
+            if (i%3==0||i%5==0||i%7==0)
+                System.out.print(i+" ");
         }//for
 
 
