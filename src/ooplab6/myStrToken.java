0557a4bdb062de3dd753e0215e534a6ca515a574
Nasri <Nasri_093654@hotmail.com>
2018-01-22 14:25:04 +0700

    StudentApp

--- src/ooplab6/myStrToken.java
+++ src/ooplab6/myStrToken.java
@@ -1,21 +1,20 @@
 package ooplab6;
 
-import jdk.nashorn.internal.ir.WhileNode;
-
 import java.util.StringTokenizer;
 
 public class myStrToken {
     public static void main(String[] args) {
-//        stringTokenizer
+//        StringTokenizer ใช้เม็ดดอทเพื่อทำการอัพ token
         String msg = "Welcome to RMUTSV";
-        StringTokenizer myToken = new myStrToken(msg);
-        System.out.println(myToken.countTokens());
-        While (myToken.hasMoreElements()){
-            myStringBuffer myBuffer = new StringBuffer(
-            myToken.nextToken());
-            System.out.println(myBuffer.reverse()+" ");
+        StringTokenizer myToken = new StringTokenizer(msg);
+        System.out.println(myToken.countTokens()); //การนับจำนวนคำ
 
+        while (myToken.hasMoreElements()){ //คำสั่งใช้อ่านคำทีละคำไปจนถึงคำสุดท้าย
+            StringBuffer myBuff = new StringBuffer(myToken.nextToken()); //nextToken การอ่านคำทีละคำ
+            System.out.println(myBuff.reverse()+" ");
         }
+
+
 
     }//main
 }//class
--- src/ooplab7/StudentApp.java
+++ src/ooplab7/StudentApp.java
@@ -2,7 +2,30 @@ package ooplab7;
 
 public class StudentApp {
     public static void main(String[] args) {
+    //create objiect as student
+        Student s1 = new Student();
+        s1.setId("111111111111");
+        s1.setName("Boy saiyai");
+        s1.setMajor("Information System");
+        s1.setGroup("IS221");
+
+        Student s2 = new Student("222222222222",
+                "Gril Saiyai",
+                "Maketing",
+                "MK221");
+        showData (s1);
+
 
 
     }//main
+
+    private static void showData(Student s) {
+        System.out.println("Student info: ");
+        System.out.println("ID: "+s.getId());
+        System.out.println("Name: "+s.getName());
+        System.out.println("Mager "+s.getMajor());
+        System.out.println("Group: "+s.getGroup());
+
+
+    }//showData
 }//class
